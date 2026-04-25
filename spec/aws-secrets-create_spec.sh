d0deedf8340147c713535bff74ec0a7ce5abc089
Charles Sena <charles.sena.sa@gmail.com>
2026-04-25 13:14:38 -0300

    test: Add failing test (About secrets creation)

---
+++ spec/aws-secrets-create_spec.sh
@@ -0,0 +1,18 @@
+Describe 'aws-secrets-create.sh'
+
+  # INFO: Prevent risky function execution
+  DRY_RUN=1
+
+  Include './aws-secrets-create.sh'
+
+  It 'test execution with expired login'
+    export AWS_STUB_EXPIRED=1
+    When call create_ghcr_secret
+
+    # INFO: Below line with `<<-EOL`, it's using tabs instead spaces
+    The stderr should equal "`cat <<-EOL
+		Your session has expired. Please reauthenticate using 'aws login'
+		EOL
+    `"
+  End
+End
